45f54ff0923b6f58aa28433ff8d42d3dddb2da1d
HaldeyC <haldeycay@gmail.com>
2025-12-25 18:12:59 +0100

    Added .js & .css file. Made imports of these in index.html. Also added error message elements in the index.html

---
+++ contact.js
@@ -0,0 +1,16 @@
+// Variables
+// Inputs
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const phoneNumber = document.getElementById("phone");
+const email = document.getElementById("email");
+const msg = document.getElementById("msg");
+
+// Outputs
+const fnameErr = document.getElementById("fname-err");
+const lnameErr = document.getElementById("lname-err");
+const phoneErr = document.getElementById("phone-err");
+const emailErr = document.getElementById("email-err");
+const txtCounter = document.getElementById("txt-counter");
+
+// Buttons
