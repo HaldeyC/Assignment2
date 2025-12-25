b1932a858f0ffc7c47f6d951f77b5d456f3e673e
HaldeyC <haldeycay@gmail.com>
2025-12-25 18:34:16 +0100

    Added all the req. functions in the .js file. Also added the dropdown option in html

--- contact.js
+++ contact.js
@@ -2,15 +2,22 @@
 // Inputs
 const firstName = document.getElementById("fname");
 const lastName = document.getElementById("lname");
-const phoneNumber = document.getElementById("phone");
 const email = document.getElementById("email");
 const msg = document.getElementById("msg");
+const subject = document.getElementById("subject");
 
 // Outputs
 const fnameErr = document.getElementById("fname-err");
 const lnameErr = document.getElementById("lname-err");
-const phoneErr = document.getElementById("phone-err");
 const emailErr = document.getElementById("email-err");
 const txtCounter = document.getElementById("txt-counter");
 
 // Buttons
+
+// Validation functions
+function validateName() {}
+function validateEmail() {}
+function validateMessage() {}
+function showError() {}
+function clearError() {}
+function clearForm() {}
